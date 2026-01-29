1b2cfea75d0d2c0347234c76e491ed3638b60255
Maren Susanne <157686972+MarenSusanne@users.noreply.github.com>
2026-01-29 23:42:26 +0100

    Update model.js

--- model.js
+++ model.js
@@ -65,49 +65,49 @@ let model = {
                         lvl: 10,
                         maxHP: 90,
                         attacks: ["Confusion", "Taunt", "After You", "Calm Mind"],
-                        img: `oranguru.png" style=width:15vw;`
+                        img: `Oranguru.png" style=width:15vw;`
                     },
                     drowzee = {
                         name: "Drowzee",
                         lvl: 21,
                         maxHP: 86,
                         attacks: ["Pound", "Hypnosis", "Headbutt", "Psybeam"],
-                        img: `drowzee.png" style=width:15vw;`
+                        img: `Drowzee.png" style=width:15vw;`
                     },
                     slowbro = {
                         name: "Slowbro",
                         lvl: 19,
                         maxHP: 125,
                         attacks: ["Tackle", "Water Gun", "Growl", "Water Pulse"],
-                        img: `slowbro.png" style=width:15vw;`
+                        img: `Slowbro.png" style=width:15vw;`
                     },
                 ],
             },
             {
                 name: "Martin",
                 lvl: 2,
-                img: `trainer2.png" style=height:15vw;`,
+                img: `Trainer2.png" style=height:15vw;`,
                 pokemons: [
                     gastly = {
                         name: "Gastly",
                         lvl: 13,
                         maxHP: 39,
                         attacks: ["Lick", "Confuse Ray", "Mean Look", "Payback"],
-                        img: `gastly.png" style=width:15vw;`
+                        img: `Gastly.png" style=width:15vw;`
                     },
                     wartortle = {
                         name: "Wartortle",
                         lvl: 12,
                         maxHP: 66,
                         attacks: ["Tackle", "Water Gun", "Rapid Spin", "Bite"],
-                        img: `wartortle.png" style=width:15vw;`
+                        img: `Wartortle.png" style=width:15vw;`
                     },
                     pidgeot = {
                         name: "Pidgeot",
                         lvl: 17,
                         maxHP: 105,
                         attacks: ["Hurricane", "Gust", "Whirlwind", "Sand Attack"],
-                        img: `pidgeot.png" style=width:15vw;`
+                        img: `Pidgeot.png" style=width:15vw;`
                     },
                 ],
             },
@@ -121,105 +121,105 @@ let model = {
                         lvl: 15,
                         maxHP: 82,
                         attacks: ["Attract", "Endeavor", "Gust", "Air Cutter"],
-                        img: `swoobat.png" style=width:15vw;`
+                        img: `Swoobat.png" style=width:15vw;`
                     },
                     bibarel = {
                         name: "Bibarel",
                         lvl: 9,
                         maxHP: 77,
                         attacks: ["Tackle", "Aqua Jet", "Defense Curl", "Rollout"],
-                        img: `bibarel.png" style=width:15vw;`
+                        img: `Bibarel.png" style=width:15vw;`
                     },
                     infernape = {
                         name: "Infernape",
                         lvl: 12,
                         maxHP: 83,
                         attacks: ["Mach Punch", "Ember", "Scratch", "Leer"],
-                        img: `infernape.png" style=width:15vw;`
+                        img: `Infernape.png" style=width:15vw;`
                     },
                 ],
             },
             {
                 name: "Rebecka",
                 lvl: 2,
-                img: `trainer3.png" style=height:15vw;`,
+                img: `Trainer3.png" style=height:15vw;`,
                 pokemons: [
                     venomoth = {
                         name: "Venomoth",
                         lvl: 15,
                         maxHP: 85,
                         attacks: ["Tackle", "Supersonic", "Air Slash", "Poison Powder"],
-                        img: `venomoth.png" style=width:15vw;`
+                        img: `Venomoth.png" style=width:15vw;`
                     },
                     butterfree = {
                         name: "Butterfree",
                         lvl: 12,
                         maxHP: 67,
                         attacks: ["Harden", "Bug Bite", "Stun Spore", "Sleep Powder"],
-                        img: `butterfree.png" style=width:15vw;`
+                        img: `Butterfree.png" style=width:15vw;`
                     },
                     dewgong = {
                         name: "Dewgong",
                         lvl: 19,
                         maxHP: 119,
                         attacks: ["Icy Wind", "Headbutt", "Growl", "Ice Shard"],
-                        img: `dewgong.png" style=width:15vw;`
+                        img: `Dewgong.png" style=width:15vw;`
                     },
                 ],
             },
             {
                 name: "Terje",
                 lvl: 2,
-                img: `trainer4.png" style=height:15vw;`,
+                img: `Trainer4.png" style=height:15vw;`,
                 pokemons: [
                     magneton = {
                         name: "Magneton",
                         lvl: 9,
                         maxHP: 51,
                         attacks: ["Tackle", "Thunder Shock", "Electric Terrain", "Thunder Wave"],
-                        img: `magneton.png" style=width:15vw;`
+                        img: `Magneton.png" style=width:15vw;`
                     },
                     charmander = {
                         name: "Charmander",
                         lvl: 8,
                         maxHP: 39,
                         attacks: ["Scratch", "Ember", "Growl", "Smokescreen"],
-                        img: `charmander.png" style=width:15vw;`
+                        img: `Charmander.png" style=width:15vw;`
                     },
                     exeggcute = {
                         name: "Exeggcute",
                         lvl: 11,
                         maxHP: 65,
                         attacks: ["Absorb", "Hypnosis", "Reflect", "Leech Seed"],
-                        img: `exeggcute.png" style=width:15vw;`
+                        img: `Exeggcute.png" style=width:15vw;`
                     },
                 ],
             },
             {
                 name: "Eskil",
                 lvl: 2,
-                img: `trainer5.png" style=height:15vw;`,
+                img: `Trainer5.png" style=height:15vw;`,
                 pokemons: [
                     paras = {
                         name: "Paras",
                         lvl: 13,
                         maxHP: 44,
                         attacks: ["Scratch", "Stun Spore", "Poison Powder", "Absorb"],
-                        img: `paras.png" style=width:15vw;`
+                        img: `Paras.png" style=width:15vw;`
                     },
                     machoke = {
                         name: "Machoke",
                         lvl: 12,
                         maxHP: 87,
                         attacks: ["Low Kick", "Leer", "Revenge", "Low Sweep"],
-                        img: `machoke.png" style=width:15vw;`
+                        img: `Machoke.png" style=width:15vw;`
                     },
                     hitmonchan = {
                         name: "Hitmonchan",
                         lvl: 4,
                         maxHP: 37,
                         attacks: ["Tackle", "Faint", "Fake Out", "Drain Punch"],
-                        img: `hitmonchan.png" style=width:15vw;`
+                        img: `Hitmonchan.png" style=width:15vw;`
                     },
                 ],
             },
@@ -233,21 +233,21 @@ let model = {
                         lvl: 9,
                         maxHP: 60,
                         attacks: ["Nuzzle", "Faint", "Spark", "Thunder"],
-                        img: `raichu.png" style=width:15vw;`
+                        img: `Raichu.png" style=width:15vw;`
                     },
                     rayquaza = {
                         name: "Rayquaza",
                         lvl: 10,
                         maxHP: 104,
                         attacks: ["Dragon Ascent", "Twister", "Air Slash", "Ancient Power"],
-                        img: `rayquaza.png" style=width:15vw;`
+                        img: `Rayquaza.png" style=width:15vw;`
                     },
                     gulpin = {
                         name: "Gulpin",
                         lvl: 19,
                         maxHP: 95,
                         attacks: ["Pound", "Sludge", "Acid Spray", "Clear Smog"],
-                        img: `gulpin.png" style=width:15vw;`
+                        img: `Gulpin.png" style=width:15vw;`
                     },
                 ],
             },
@@ -260,6 +260,7 @@ let model = {
         ]
     },
 }
+
 
 
 
